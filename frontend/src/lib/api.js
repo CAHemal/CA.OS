d49ec96b6c2f144b5439f91556f16aaa0fec238d
CAHemal <ca.ip.hemal@gmail.com>
2026-03-14 20:20:58 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+  baseURL: `baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
