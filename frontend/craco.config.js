d51325e2999817a430f8aa8bddd0f1aff1edc021
CAHemal <ca.ip.hemal@gmail.com>
2026-03-19 10:54:03 +0530

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,18 +2,32 @@ const path = require("path");
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Disable source maps for smaller builds
+      // Disable source maps
       webpackConfig.devtool = false;
 
-      // Remove ESLintWebpackPlugin completely
+      // Remove ESLintWebpackPlugin
       webpackConfig.plugins = webpackConfig.plugins.filter(
         (plugin) => plugin.constructor.name !== "ESLintWebpackPlugin"
       );
 
-      // Add alias
+      // Alias
       webpackConfig.resolve.alias = {
         ...(webpackConfig.resolve.alias || {}),
         "@": path.resolve(__dirname, "src"),
+      };
+
+      // Reduce Terser parallelism to save memory
+      const terser = webpackConfig.optimization.minimizer.find(
+        (m) => m.constructor.name === "TerserPlugin"
+      );
+      if (terser) {
+        terser.options.parallel = 1;
+      }
+
+      // Simpler chunk splitting
+      webpackConfig.optimization.splitChunks = {
+        chunks: "all",
+        maxSize: 200000,
       };
 
       return webpackConfig;
